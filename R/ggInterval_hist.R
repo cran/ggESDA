9a92fd891ab77dcefc37d1507bc526d835cc5b7e
Bo-Syue Jiang <lukajiang1998@gmail.com>
2022-01-10 09:02:42 +0000

    version 0.1.0

---
+++ R/ggInterval_hist.R
@@ -0,0 +1,282 @@
+#' @name ggInterval_hist
+#' @title Histogram for symbolic data with equal-bin or unequal-bin.
+#' @description  Visualize the continuous variable distribution
+#' by dividing the x axis into bins,and calculating the frequency
+#' of observation interval in each bin.
+#' @import rlang ggplot2 tidyverse
+#' @importFrom RSDA is.sym.interval
+#' @param data A ggESDA object.It can also be either RSDA object or
+#' classical data frame,which will be automatically convert to ggESDA
+#' data.
+#' @param mapping Set of aesthetic mappings created by aes() or aes_().
+#' If specified and inherit. aes = TRUE (the default),
+#' it is combined with the default mapping at the top level of
+#' the plot. You must supply mapping if there is no plot mapping.
+#' It is the same as the mapping of ggplot2.
+#' @param method It can be equal-bin(default) or unequal-bin.Enqual-bin means
+#' the width in histogram is equal, which represent all intervals divided
+#' have the same range. unequal-bin means the range of intervals are not
+#' the same,and it can be more general on data. Thus, the bins of unequal-bin
+#' method depends on the data, and the argument "bins" will be unused.
+#' @param bins x axis bins,which mean how many partials the variable
+#' @param plotAll boolean, whether plot all variables, default FALSE.
+#' will be separate into.
+#' @return Return a ggplot2 object.
+#' @usage ggInterval_hist(data = NULL,mapping = aes(NULL),method="equal-bin",bins=10,
+#'  plotAll = FALSE)
+#' @examples
+#' ggInterval_hist(mtcars,aes(x=wt))
+#'
+#' ggInterval_hist(iris,aes(iris$Petal.Length,col="blue",alpha=0.2,
+#'    fill="red"),bins=30)
+#'
+#'
+#' d<-data.frame(x=rnorm(1000,0,1))
+#' p<-ggInterval_hist(d,aes(x=x),bins=40,method="equal-bin")
+#' p
+#'
+#' p+scale_fill_manual(values=rainbow(40))+labs(title="myNorm")
+#'
+#' @export
+ggInterval_hist<-function(data = NULL,mapping = aes(NULL),method="equal-bin",bins=10,
+                          plotAll = FALSE){
+  #data preparing
+  argsNum<-length(mapping)
+  args<-lapply(mapping[1:argsNum],FUN=rlang::get_expr)
+  this.x <- args$x ; this.y <- args$y
+
+  #test data illegal
+  ggSymData <- testData(data)
+  iData <- ggSymData$intervalData
+  if(plotAll){
+    if(!is.null(this.x) | !is.null(this.y)){
+      warning("Using plotAll presentation cannot specify variables.")
+    }
+  }else{
+    testXY(iData,this.x,this.y)
+  }
+  p<-dim(iData)[2]
+  n<-dim(iData)[1]
+
+  #test figure clearly
+  if(method=="equal-bin" & bins>200 & bins<=1000){
+    warning("Bins are too large that will lead the figure to be fuzzy.")
+  }else if(method=="equal-bin" & bins>1000){
+    stop("Bins are too large that will lead the figure to be fuzzy.")
+  }
+
+
+  if("ggESDA" %in% class(data)){data <- iData}
+  #start process
+  with(data,{
+    plotVarNum <- NULL
+    mmd <- data.frame(NULL) #min and max data frame
+    if(plotAll){
+      #get numerical data
+      numericData <- unlist(lapply(data.frame(iData[1:dim(iData)[2]]) ,FUN = is.sym.interval))
+      iData <- iData[,which(numericData)]
+      attr <- colnames(iData)
+
+      for(i in 1:length(attr)){
+        mmd[i, "minimal"]<-min(iData[[i]]$min)
+        mmd[i, "maximal"]<-max(iData[[i]]$max)
+      }
+    }else{
+
+      #get attr
+      if(any(unlist(lapply(as.data.frame(data[,1:p]),FUN=identical,x=eval(this.x))))){
+        attr<-which(unlist(lapply(as.data.frame(data[,1:p]),FUN=identical,x=eval(this.x))))
+        attr<-names(attr)
+      }else if(any(unlist(lapply(as.data.frame(data[,1:p]),FUN=identical,x=eval(this.y))))){
+        attr<-which(unlist(lapply(as.data.frame(data[,1:p]),FUN=identical,x=eval(this.y))))
+        attr<-names(attr)
+      }else{
+        stop("ERROR : Cannot find variables in aes(...)")
+      }
+      if(p==1){
+        attr = colnames(data)
+      }
+
+      #test attribute illegal
+      if(all(!is.numeric(data[[attr]]) , !RSDA::is.sym.interval(data[[attr]]))){
+        stop("ERROR : Variables in histogram can only be numeric.")
+      }
+      mmd[1, "minimal"]<-min(iData[[attr]]$min)
+      mmd[1, "maximal"]<-max(iData[[attr]]$max)
+
+    }#end plotAll
+
+    if(method=="unequal-bin"){
+
+      plotData <- NULL ; intervalDf = NULL
+      for(u in 1:nrow(mmd)){
+        d<-as.data.frame(iData[[attr[u]]])
+        temp<-seq(mmd[u, "minimal"], mmd[u, "maximal"],0.01)
+        f <- matrix(0,nrow=length(temp),ncol=n+1)
+        f[,1]<-temp
+
+        #calculate fraquency
+        for (i in 1:n){
+          #start debug version0825-10
+          #raw code :
+          #start<-which(round(f[,1],2)==round(d[i,1],2))
+          #end<-which(round(f[,1],2)==round(d[i,2],2))
+          #new code :
+          start<-length(which(round(f[,1],2)<=round(d[i,1],2)))
+          end<-length(which(round(f[,1],2)<=round(d[i,2],2)))
+          #end debug
+
+          f[start:end,i+1]<-1
+        }
+
+        f<-cbind(temp,apply(f[,-1],1,FUN=sum))
+
+        x<-f[1,2]
+        y<-1
+        for (i in 1:(dim(f)[1]-1)){
+          if(f[i,2]!=f[i+1,2]){
+            y<-append(y,f[i+1,2])
+            x<-append(x,i)
+            x<-append(x,i+1)
+          }
+        }
+        x<-append(x,length(temp))
+        x<-t(matrix(x,nrow=2))
+        x1<-f[x[,1],1]
+        x2<-f[x[,2],1]
+        plotData<-rbind(plotData,
+                        data.frame(x1,x2,y1=0,y2=y/n,group=factor(attr[u])))
+
+        tempDf <- data.frame(start = rep(NA, length(x1)),
+                             end = rep(NA, length(x1)))
+        tempDf[1:length(x1), "start"] <- round(x1,2)
+        tempDf[1:length(x1), "end"] <- round(x2,2)
+        intervalDf <- rbind(intervalDf, tempDf)
+      }
+
+      intervalDf <- intervalDf[order(intervalDf$start), ]
+      temp <- paste(intervalDf$start, intervalDf$end, sep = ":")
+      nameList <- paste0("[",temp,"]")
+
+      midP <- (plotData$x1 + plotData$x2)/2
+      #build Aesthetic (mapping)
+      xyLocation <- c(which(names(mapping) == "x"), which(names(mapping) == "y"))
+      if(length(xyLocation) != 0){
+        usermapping <- mapping[-xyLocation] #Aesthetic without x,y
+      }else{
+        usermapping <- mapping
+      }
+      mymapping <- list(mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2),fill="grey",col="black")
+      allmapping <-as.list(structure(as.expression(c(usermapping,mymapping)),class="uneval"))
+
+      #plot
+      base <- ggplot(data=plotData,aes(x1,y1))+
+        do.call(geom_rect,allmapping)+
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+      if(plotAll){
+        base <- base + facet_grid(group ~ .) +
+          labs(x = "", y="frequency") +
+          scale_x_continuous(n.breaks = 8)
+      }else{
+        base <- base + labs(x=attr,y="frequency")+
+          scale_x_continuous(breaks = midP, labels=nameList)
+      }
+      return(base)
+
+    }else if(method=="equal-bin"){
+      myhist <- NULL ; intervalDf <- NULL
+      for(i in 1:nrow(mmd)){
+        #seperate the attribute into bins
+        dist<-(mmd[i, "maximal"]-mmd[i, "minimal"])/bins
+        interval<-seq(mmd[i, "minimal"],mmd[i, "maximal"],dist)
+
+
+        #calculate frequency
+        f <- NULL
+        f <- matrix(nrow=length(iData[[attr[i]]]),ncol=bins)
+        for (obs in 1:length(iData[[attr[i]]])){
+          a<-iData[[attr[i]]][obs]$min
+          b<-iData[[attr[i]]][obs]$max
+          for(area in 1:bins){
+            headIn<-a %>% between(interval[area],interval[area+1])
+            tailIn<-b %>% between(interval[area],interval[area+1])
+            contain<-interval[area] %>% between(a,b)
+            if(headIn|tailIn|contain){
+              temp<-sort(c(a,b,interval[area],interval[area+1]))
+              f[obs,area]<-(temp[3]-temp[2])/(b-a)
+            }else{
+              f[obs,area]<-0
+            }
+          }
+        }
+
+        nInt <- length(interval)
+        interval <- round(interval,2)
+        tempDf <- data.frame(start = rep(NA, nInt-1),
+                             end = rep(NA, nInt-1))
+        tempDf[1:(nInt-1), "start"] <- interval[1:(nInt-1)]
+        tempDf[1:(nInt-1), "end"] <- interval[2:nInt]
+        intervalDf <- rbind(intervalDf, tempDf)
+
+        #build data frame and plot
+        myhist<-rbind(myhist,
+                      data.frame(interval=interval[1:bins],
+                           frequency=apply(f,2,FUN=sum),
+                           group = factor(attr[i])))
+
+      }
+      myhist <- cbind(myhist, intervalDf)
+      intervalDf <- intervalDf[order(intervalDf$start), ]
+      temp <- paste(intervalDf$start, intervalDf$end, sep = ":")
+      #temp<-mapply(intervalDf$start,intervalDf$end,FUN=function(x,y) paste(interval[x],interval[y],sep = ":"))
+      nameList <- paste0("[",temp,"]")
+
+
+      myhist$frequency <- myhist$frequency/n
+
+
+      #build Aesthetic (mapping)
+      #
+      xyLocation <- c(which(names(mapping) == "x"), which(names(mapping) == "y"))
+      if(length(xyLocation) != 0){
+        usermapping <- mapping[-xyLocation] #Aesthetic without x,y
+      }else{
+        usermapping <- mapping
+      }
+      mymapping <- list(stat="identity",
+                        mapping=aes(alpha=0.5,fill=gray.colors(bins*length(attr))),col="black")
+      allmapping <-as.list(structure(as.expression(c(usermapping,mymapping)),class="uneval"))
+
+
+      #plot
+      if(plotAll){
+        mymapping <- list(mapping=aes(xmin=start, xmax=end, ymin=0, ymax=frequency),fill="grey",col="black")
+        allmapping <-as.list(structure(as.expression(c(usermapping,mymapping)),class="uneval"))
+        base <- ggplot(data=myhist)+
+          do.call(geom_rect,allmapping) +
+          facet_grid(group ~ .) +
+          labs(x = "", y="frequency") +
+          scale_x_continuous(n.breaks = 8)
+
+      }else{
+        base <- ggplot(data=myhist, aes(x=factor(interval),y=frequency))+
+          do.call(geom_histogram,allmapping)+
+          scale_fill_manual(values=rep("black",bins*length(attr)))+
+          guides(colour = FALSE, alpha = FALSE,fill=FALSE)+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(x=attr)+
+          scale_x_discrete(labels = nameList)
+      }
+      return(base)
+
+    }else{
+      stop(paste0("ERROR : Unrecognize method : ",method," ."))
+    }
+  })
+}
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,207 @@
+# ggESDA : An R package for Exploratory Symbolic data analysis
+
+Symbolic data analysis (SDA) is an extension of standard data analysis where symbolic data tables are used as input and symbolic objects are made output as a result. The data units are called symbolic since they are more complex than standard ones, as they not only contain values or categories, but also include internal variation and structure.<a href="#ref1">[1]</a><a href="#ref2">[2]</a>
+
+<b>ggESDA</b> is an extension of ggplot2 for visualizing the symbolic data based on exploratory data analysis (EDA). The package contains many useful graphic techniques functions. Furthermore, the users can also transform the classical data into the symbolic data by the function in this package, which is one of the most important processing in SDA. For the details of the package study, you can see in <a href="https://github.com/kiangkiangkiang/RESEARCH/blob/master/ggESDA_Jiang%26Wu_20210915.pdf">Jiang (2022)</a>.
+
+## Package installation
+
+`devtools::install_github("kiangkiangkiang/ggESDA")`
+
+## Creating
+
+The example data is called <b>facedata</b> </a><a href="#ref3">[3]</a>. It will be the interval form with minimal and maximal, known as <b>interval-valued</b> data. However, most of the symbolic data are not exist at the beginning. Instead, they are usually aggregated by clustering algorithm from a classical data. Thus, we will use `classic2sym()` function to summarize classical data into symbolic data.
+
+
+```{r library}
+library(ggESDA)
+```
+
+```{r createData}
+#aggregated by the variable Species in iris
+iris_interval_var <- classic2sym(iris, groupby = Species)
+iris_interval_kmeans <- classic2sym(iris, groupby = "kmeans")
+iris_interval_hclust <- classic2sym(iris, groupby = "hclust")
+
+d <- data.frame(minData = runif(100, 0, 10),
+                maxData = runif(100, 50, 60))
+                
+
+d.sym <- classic2sym(d, groupby = "customize",
+                    minData = d$minData,
+                    maxData = d$maxData)
+                    
+#get interval-valued data
+d.sym$intervalData
+```
+
+## Visualization
+
+With the symbolic data generated, you can start to visualize the data by the following functions:
+
+### ggInterval_index() for visualizing the interval of each observations
+
+
+<img src = "vignettes/images/ggInterval_index2.png" align = "right"></img>
+
+```{r ggInterval_index,eval=FALSE}
+# get the mean value for the hline
+m <- mean(facedata$AD)
+
+# build a color mapping for each person
+Concepts <- as.factor(rep(c("FRA", "HUS", "INC", "ISA", "JPL", "KHA",
+                            "LOT", "PHI", "ROM"), each = 3))
+                            
+# start plot
+ggInterval_index(facedata, aes(x = AD, fill = Concepts))+
+  theme_bw() +
+  scale_fill_brewer(palette = "Set2")+
+  geom_segment(x = m, xend = m, y = 0, yend = 27,
+               lty = 2, col = "red", lwd = 1) +
+  geom_text(aes(x = m, y = 28), label = "Mean")+
+  scale_y_continuous(breaks = 1:27,
+                     labels = rownames(facedata))
+                     
+```
+It can get the preliminary understanding of the interval-valued data. 
+
+You can also change `fill =` and `col =` to make the plot more visible, and set x or y axis to your variable will rotate the index line in figure.
+
+### ggInterval_minmax() for visualizing the interval of each observations which is sorted by minimal value
+
+```{r ggInterval_minmax,eval=FALSE}
+ggInterval_minmax(data = NULL, mapping = aes(NULL), scaleXY = "local", plotAll = F)
+```
+MMplot is an advanced graphics implemented for symbolic data, or interval-valued data. It presents the interval by the minimum and maximum, and shows the difference between each location of concept in each variable through the 45-degree line. The options `scaleXY = "local"` will define the axis limit for the comparsion.
+
+### ggInterval_boxplot() for visualizing the distribution of the interval data
+
+```{r ggInterval_boxplot,eval=FALSE}
+ggInterval_boxplot(facedata, plotAll = T) + theme_bw()
+```
+<img src = "vignettes/images/ggInterval_boxplot.png" width = "75%"></img>
+
+The side-by-side boxplot clearly shows the difference of variables' distribution.
+
+### ggInterval_hist() for visualizing the distribution of the interval data
+
+For interval-valued data, not only equidistant-bin histogram but the Non-equidistant-bin histogram will exhibit the distribution. Use the option `method`, and set by `equal-bin` or `unequal-bin`. See the details <a href="https://github.com/kiangkiangkiang/RESEARCH/blob/master/ggESDA_Jiang%26Wu_20210915.pdf">Jiang (2022)</a>.
+
+```{r ggInterval_hist,eval=FALSE}
+equal_bin <- ggInterval_hist(facedata, plotAll = T) +
+              theme_bw()
+unequal_bin <- ggInterval_hist(facedata, plotAll = T,
+                               method = "unequal-bin") +
+              theme_bw()
+ggarrange(equal_bin, unequal_bin, ncol = 2)  
+```
+<img src = "vignettes/images/ggInterval_hist.png" width = "75%"></img>
+
+### ggInterval_centerRange for visualizing the relation between center and range of the interval of each observations.
+
+```{r ggInterval_minmax,eval=FALSE}
+ggInterval_centerRange(data = NULL, mapping = aes(NULL), plotAll = F)
+```
+Another advanced graphics implemented is called center-range plot, which helps researchers to be able to grasp the relationship between center and range.
+
+### ggInterval_scatter for visualizing the the relation of two variables
+
+A scatter plot of interval-valued data is presented by a rectangle, which is composed of two interval.
+
+As well, ggInterval_scaMatrix is an extension for visualizing all variables relations by using scatter plot at a time.
+
+```{r ggInterval_minmax,eval=FALSE}
+ggInterval_scaMatrix(facedata)
+```
+<img src = "vignettes/images/ggInterval_scaMatrix.png" width = "75%"></img>
+
+
+### ggInterval_2Dhist for visualizing the two variable distribution by theirs frequency of interval bins
+
+Another bivariate relationship plot is two-dimension histogram, which is to calculate joint
+histogram frequency and using matrix visualization to plot.
+
+By the extension, ggInterval_2DhistMatrix is to visualize all variables relations by using 2Dhist plot at a time
+
+```{r ggInterval_2DhistMatrix,eval=FALSE}
+ggInterval_2DhistMatrix(facedata,
+                        xBins = 10,
+                        yBins = 10,
+                        removeZero = T,
+                        addFreq = F)
+```
+
+<img src = "vignettes/images/ggInterval_2DhistMatrix.png" width = "75%"></img>
+
+### ggInterval_indexImage for visualizing the interval of each observations by using image
+
+A heatmap type presentation for the interval by the option `plotAll` and `full_strip`. There will be two distinct type, column condition and matrix condition.  See the details <a href="https://github.com/kiangkiangkiang/RESEARCH/blob/master/ggESDA_Jiang%26Wu_20210915.pdf">Jiang (2022)</a>.
+
+```{r ggInterval_indexImage2,eval=FALSE}
+p1 <- ggInterval_indexImage(facedata, plotAll = T, column_condition = T,
+                      full_strip = T)
+
+p2 <- ggInterval_indexImage(facedata, plotAll = T, column_condition = F,
+                      full_strip = T)
+
+ggpubr::ggarrange(p1, p2, ncol = 2)
+
+```
+
+<img src = "vignettes/images/ggInterval_indexImage2.png" width = "75%"></img>
+
+### ggInterval_radar for visualizing the interval of multivariates
+
+One of the most well-known multivariate visualization techniques is radar plot, or called start plot. We fill the interval area by color mapping, and compare CONCEPTs, surely you can arrange it.
+
+In `ggInterval_radar`, you can add any annotations in figure, including a circle for classify the normalize data position, a real value for the interval or a propotion for modal multi-valued variables.
+
+<img src = "vignettes/images/ggInterval_radar.png" width = "75%"></img>
+
+Surely, we always compare the data in the same figure. Just use the color mapping for the different observations.
+
+<img src = "vignettes/images/ggInterval_radar2.png" width = "75%"></img>
+
+In the field of interval-valued data, the quantile is usually for analysis. We can present the quantiles for each datasets.
+
+<img src = "vignettes/images/ggInterval_radar3.png" width = "75%"></img>
+
+
+### ggInterval_PCA for dimension reduction in interval data
+
+Two kinds of dimension reduction,  see the details <a href="https://github.com/kiangkiangkiang/RESEARCH/blob/master/ggESDA_Jiang%26Wu_20210915.pdf">Jiang (2022)</a>, show in following:
+
+```{r ggInterval_radar2,eval=FALSE}
+CONCEPT <- rep(c("FRA", "HUS", "INC", "ISA", "JPL", "KHA",
+           "LOT", "PHI", "ROM"), each = 3)
+p <- ggInterval_PCA(facedata, poly = T,
+                    concepts_group = CONCEPT)
+p$ggplotPCA <- p$ggplotPCA + theme(legend.position = "top") + 
+  theme_bw() 
+
+p2 <- ggInterval_PCA(facedata, poly = F,
+                    concepts_group = CONCEPT)
+p2$ggplotPCA <- p2$ggplotPCA + theme(legend.position = "top") +
+  theme_bw()
+
+ggpubr::ggarrange(p$ggplotPCA, p2$ggplotPCA, ncol = 2)
+```
+
+<img src = "vignettes/images/ggInterval_PCA.png" width = "75%"></img>
+
+
+
+## References
+<p id="ref1">
+1. Diday, Edwin; Esposito, Floriana (December 2003). "An introduction to symbolic data analysis and the SODAS software".
+</p>
+
+<p id="ref2">
+2. Lynne Billard; Edwin Diday (14 May 2012). Symbolic Data Analysis: Conceptual Statistics and Data Mining.
+</p>
+
+<p id="ref3">
+ 3. Billard L. and Diday E. (2006). Symbolic data analysis: Conceptual statistics and data mining. Wiley, Chichester.
+ </p>
+ 
+
